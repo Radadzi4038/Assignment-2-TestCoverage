d009b8f2cdc67908dbaef5e5ef11297e7f0f6169
Radadzi4038 <radadzi4038@conestogac.on.ca>
2025-06-15 16:42:53 -0400

    Improving production code to pass the "throw error for invalid string input" test

--- SENG8120-Modern-Automated-Testing-evaluating-for-test-coverage/src/calculateCanvasSize/calculateCanvasSize.ts
+++ SENG8120-Modern-Automated-Testing-evaluating-for-test-coverage/src/calculateCanvasSize/calculateCanvasSize.ts
@@ -1,6 +1,10 @@
-export default function calculateCanvasSize(
-  length: string,
-  width: string
-) {
-  return parseInt(length) * parseInt(width);
+export default function calculateCanvasSize(length: string, width: string) {
+  const len = parseInt(length);
+  const wid = parseInt(width);
+
+  if (isNaN(len) || isNaN(wid)) {
+    throw new Error("Invalid input");
+  }
+
+  return len * wid;
 }
